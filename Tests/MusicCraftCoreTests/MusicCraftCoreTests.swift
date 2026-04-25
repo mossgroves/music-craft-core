f2cab7ae571d70c531f4910a539d6c562d5bf783
Chris Weber <852305+cweb@users.noreply.github.com>
2026-04-24 22:03:11 -0700

    test: update version test to 0.0.7

--- Tests/MusicCraftCoreTests/MusicCraftCoreTests.swift
+++ Tests/MusicCraftCoreTests/MusicCraftCoreTests.swift
@@ -3,6 +3,6 @@ import XCTest
 
 final class MusicCraftCoreTests: XCTestCase {
     func testVersionIsSet() {
-        XCTAssertEqual(musicCraftCoreVersion, "0.0.3")
+        XCTAssertEqual(musicCraftCoreVersion, "0.0.7")
     }
 }
